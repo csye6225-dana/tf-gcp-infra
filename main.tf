c998e94b0f368f652991dbcaffd496d2654f966f
DanaKwoh <guo0214@126.com>
2024-02-14 21:02:20 -0500

    update

--- main.tf
+++ main.tf
@@ -12,89 +12,60 @@ terraform {
 provider "google" {
   credentials = file(var.credentials_file)
   project     = var.project
-  region      = var.region
+  region      = "us-central1"
 }
 
 # Create a VPC network
 resource "google_compute_network" "vpc_network" {
-  name                  = var.network
+  name                  = "terraform-network"
   auto_create_subnetworks = false
   routing_mode          = "REGIONAL"
 }
 
 # Create a subnet for webapp
 resource "google_compute_subnetwork" "webapp_subnet" {
-  name          = var.subnet1
+  name          = "webapp"
   ip_cidr_range = "10.0.1.0/24"
   network       = google_compute_network.vpc_network.self_link
-  region        = var.region
+  region        = "us-central1"
 }
 
 # Create a subnet for db
 resource "google_compute_subnetwork" "db_subnet" {
-  name          = var.subnet2
+  name          = "db"
   ip_cidr_range = "10.0.2.0/24"
   network       = google_compute_network.vpc_network.self_link
-  region        = var.region
+  region        = "us-central1"
 }
 
 # Create a router
 resource "google_compute_router" "my_router" {
-  name    = var.router
+  name    = "my-router"
   network = google_compute_network.vpc_network.self_link
+}
+
+# Create a global address for the NAT gateway
+resource "google_compute_global_address" "nat_address" {
+  name = "nat-address1"
+}
+
+# Create a Cloud NAT gateway
+resource "google_compute_router_nat" "nat_gateway" {
+  name                        = "nat-gateway"
+  router                      = google_compute_router.my_router.name
+  region                      = "us-central1"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  nat_ip_allocate_option      = "MANUAL_ONLY"
+  nat_ips                     = [google_compute_global_address.nat_address.address]  # Use the address from the global address resource
+  depends_on                  = [google_compute_router.my_router]
 }
 
 # Create a route for the webapp subnet
 resource "google_compute_route" "webapp_route" {
-  name              = var.route
+  name              = "webapp-route"
   dest_range        = "0.0.0.0/0"
   network           = google_compute_network.vpc_network.self_link
-  next_hop_gateway  = "default-internet-gateway"
+  next_hop_gateway  = google_compute_router.my_router.self_link
   priority          = 1000  # Set priority higher to ensure it's preferred over default route
-  depends_on        = [google_compute_subnetwork.webapp_subnet]
-}
-
-
-# Create firewall rules
-resource "google_compute_firewall" "webapp_firewall" {
-  name    = "webapp-firewall"
-  network = google_compute_network.vpc_network.self_link
-  allow {
-    protocol = "tcp"
-    ports    = ["8080"]
-  }
-  source_ranges = ["0.0.0.0/0"] # Allow traffic from the internet
-}
-
-
-resource "google_compute_firewall" "deny_ssh" {
-  name    = "deny-ssh"
-  network = google_compute_network.vpc_network.self_link
-  deny {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-  source_ranges = ["0.0.0.0/0"]  # Deny traffic from the internet
-}
-
-
-# Create a Compute Engine instance based on image
-resource "google_compute_instance" "web_server" {
-  name         = "web-server"
-  machine_type = "n1-standard-2"
-  zone         = "us-central1-a"
-  tags         = ["http-server"]
-
-  boot_disk {
-    initialize_params {
-      image = "custom-image"
-      size  = "100"
-      type  = "pd-balanced"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.self_link
-  }
-  metadata_startup_script = "sudo systemctl start node app.js"
+  depends_on        = [google_compute_subnetwork.webapp_subnet, google_compute_router_nat.nat_gateway]
 }
