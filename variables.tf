2c4ca0b40e15cbd5384a2752abeca64d82a49638
DanaKwoh <danag@myhost.local>
2024-02-22 15:11:11 -0500

    update

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "project" {
-    default = "csye6225-cloudcomputing" 
+    default = "cloudcomputing-415019"
 }
 
 variable "credentials_file" { 
@@ -23,7 +23,7 @@ variable "ip_range2"{
 }
 
 variable "router"{
-    default = "router2"
+    default = "router"
 }
 
 variable "route"{
@@ -44,4 +44,64 @@ variable "region" {
 
 variable "zone" {
   default = "us-central1-c"
+}
+
+variable "auto_create"{
+    default = false
+}
+
+variable "next_gateway"{
+    default = "default-internet-gateway"
+}
+
+// compute engine instance //
+variable "service_account"{
+    default = "1025734219928-compute@developer.gserviceaccount.com"
+}
+variable "server_name"{
+    default = "web-server"
+}
+
+variable "machine_type"{
+    default = "n1-standard-2"
+}
+
+variable "server_tag"{
+    default = ["http-server"]
+}
+
+variable "image"{
+    default = "custom-image"
+}
+
+variable "image_size"{
+    default = 100
+}
+
+variable "image_type"{
+    default = "pd-balanced"
+}
+
+variable "startup_script"{
+    default = "cp -r /tmp/webapp /home/danakwoh"
+}
+
+# firewalls
+variable "firewall1"{
+    default = "webapp-firewall"
+}
+variable "firewall2"{
+    default = "ssh-firewall"
+}
+variable "allow_port"{
+    default = ["8080"]
+}
+variable "protocol"{
+    default = "tcp"
+}
+variable "deny_port"{
+    default = ["22"]
+}
+variable "source_ranges"{
+    default = "0.0.0.0/0"
 }
