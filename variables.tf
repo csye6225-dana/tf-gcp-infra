16ea76513e8d76a89a958f8c250c6fc54d9661a9
Dana <80471259+DanaKwoh@users.noreply.github.com>
2024-02-26 02:14:20 -0500

    demo (#4)
    * update
    
    * final version 1.0
    
    * Delete .terraform.lock.hcl
    
    * Delete .terraform.tfstate.lock 2.info
    
    * Delete credentials.json
    
    * Delete terraform.tfstate
    
    * Delete variables.tf
    
    * Delete terraform.tfstate.backup
    
    * Delete .terraform/providers/registry.terraform.io/hashicorp directory
    
    * update
    
    * update
    
    * Update README.md
    
    * Update validation.yml
    
    * update
    
    * Update main.tf
    
    * update
    
    * Update main.tf
    
    * Update main.tf
    
    * update
    
    * Delete credentials.json
    
    * Delete terraform.tfstate
    
    * Delete .terraform.lock.hcl
    
    * Update .gitignore
    
    * uodate
    
    * update packer
    
    * update
    
    * update instance network setting
    
    * Update main.tf
    
    * variable
    
    * Update .gitignore
    
    * Update variables.tf
    
    * final version
    
    * Update validation.yml
    
    ---------
    
    Co-authored-by: DanaKwoh <danag@myhost.local>

--- variables.tf
+++ variables.tf
@@ -1,21 +1,37 @@
-variable "project" {
-    default = "csye6225-cloudcomputing" 
+variable "project_id" {
+    default = "cyse6225-cloudcomputing-webapp"
 }
 
 variable "credentials_file" { 
     default = "credentials.json"
 }
+// compute engine instance //
+variable "service_account"{
+    default = "960667116773-compute@developer.gserviceaccount.com"
+}
 
 variable "network"{
-    default = "vpc-network1"
+    default = "terraform-vpc"
+}
+
+variable "routing_mode"{
+    default = "REGIONAL"
+}
+
+variable "ip_range1"{
+    default = "198.162.1.0/24"
+}
+
+variable "ip_range2"{
+    default = "10.0.2.0/24"
 }
 
 variable "router"{
-    default = "router1"
+    default = "router"
 }
 
 variable "route"{
-    default = "route1"
+    default = "route"
 }
 
 variable "subnet1"{
@@ -32,4 +48,63 @@ variable "region" {
 
 variable "zone" {
   default = "us-central1-c"
+}
+
+variable "auto_create"{
+    default = false
+}
+
+variable "next_gateway"{
+    default = "default-internet-gateway"
+}
+
+
+variable "server_name"{
+    default = "web-server"
+}
+
+variable "machine_type"{
+    default = "n1-standard-2"
+}
+
+variable "server_tag"{
+    default = ["http-server"]
+}
+
+variable "image"{
+    default = "custom-image"
+}
+
+variable "image_size"{
+    default = 100
+}
+
+variable "image_type"{
+    default = "pd-balanced"
+}
+
+# firewalls
+variable "firewall1"{
+    default = "webapp-firewall"
+}
+variable "firewall2"{
+    default = "ssh-firewall"
+}
+variable "allow_port"{
+    default = ["8080"]
+}
+variable "protocol"{
+    default = "tcp"
+}
+variable "deny_port"{
+    default = ["22"]
+}
+variable "source_ranges"{
+    default = "0.0.0.0/0"
+}
+variable "startup_script" {
+  default = "cd /opt/csye6225 && node app.js"
+}
+variable "service_scope" {
+  default = ["cloud-platform"]
 }
